48b85d3f75c3c4430371bfd8fbc4214ed23a18f4
David Saunders <david@monkee-boy.com>
2017-11-29 09:49:14 -0600

    Created books reducer function

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+// A reducer is a function that returns a piece of the application's state
+// Can have multiple reducers
+
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
